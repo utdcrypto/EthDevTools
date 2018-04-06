06c955e41e251fe3b4d7e3ff1236f829fc1fa1be
no_data_here <error.404.no.data.here@gmail.com>
2018-04-06 15:17:50 -0500

    updated toolkit and header comment

--- ethersetup.sh
+++ ethersetup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#this script effectively runs through a basic manual install of the tools listed as required in https://gist.github.com/cbailey555/05b36d8f3ad868ec0a67f69a24c7f9b0
+#this script installs a number of tools for ethereum developers
 #script by no_data_here <error.404.no.data.here@gmail.com>
 
 #add etherium ppa and make sure that all software installed via apt is prepared
-sudo apt install build-essential;
+sudo apt install build-essential software-properties-common;
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt update;
 sudo apt upgrade -y;
 sudo apt update;
 sudo apt upgrade -y;
-sudo apt install solc;
-sudo apt install curl;
-sudo apt install npm;
-sudo apt install git;
+sudo apt install solc curl npm git geth;
+
+#install rust via rustup
+curl https://sh.rustup.rs -sSf | sh;
+
+#install parity
+bash <(curl https://get.parity.io -Lk)
 
 #install npm via method reccommended at https://github.com/creationix/nvm#installation
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash;
@@ -22,13 +25,16 @@ export NVM_DIR="$HOME/.nvm"
 #install latest nodejs
 nvm install node;
 #install reccommended web3
-npm install web3@0.18.2;
+npm install web3;
 #install testrpc utility
 npm install -g ganache-cli;
 #install truffle suite/debugger
 npm install -g truffle;
 
 #clones workshops from http://github.com/utdcrypto to folders named by date of workshops
-x-terminal-emulator -e "curl https://raw.githubusercontent.com/utdcrypto/EthDevTools/master/cloneworkshops.sh | bash"&
+wget https://raw.githubusercontent.com/utdcrypto/EthDevTools/master/cloneworkshops.sh /home/$USER/cloneworkshops.sh;
+bash /home/$USER/cloneworkshops.sh;
+rm /home/$USER/cloneworkshops.sh;
+
 
 exit;
